0375560b8b015548d0dcbad699d03c1ebd16ff26
nasser <nasseritani26@gmail.com>
2019-01-21 16:41:14 +0300

    update

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port=process.env.PORT || 3000
 const app = express();
 var username = "sfadmin@SFPART026942",
     password = "welcome",
+    urls="https://apisalesdemo2.successfactors.eu/odata/v2/Photo(photoType=1,userId='sfadmin')",
     url = "https://apisalesdemo2.successfactors.eu/odata/v2/User('sfadmin')",
     auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
 app.listen(port);
@@ -17,11 +18,30 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 app.get('/rates',function(req,res){
-    console.log(req);
+    //console.log(req);
     const requestOptions = {
   method: 'GET',
-  uri: url,
+  uri: urls,
   headers: {
+    "Authorization" : auth
+  },
+  json: true,
+  gzip: true
+};
+
+rp(requestOptions).then(response => {
+  //console.log('API call response:', response);
+    res.send(response);
+}).catch((err) => {
+  console.log('API call error:', err.message);
+});})
+
+app.get('/Employee/:id',function(req,res){
+  console.log(req.params.id);
+     const requestOptions = {
+  method: 'GET',
+  uri:"https://apisalesdemo2.successfactors.eu/odata/v2/User('"+req.params.id+"')",
+   headers: {
     "Authorization" : auth
   },
   json: true,
@@ -33,4 +53,24 @@ rp(requestOptions).then(response => {
     res.send(response);
 }).catch((err) => {
   console.log('API call error:', err.message);
-});})
+});
+})
+app.get('/Employee/:id/1',function(req,res){
+  console.log(req.params.id);
+     const requestOptions = {
+  method: 'GET',
+  uri:"https://apisalesdemo2.successfactors.eu/odata/v2/Photo(photoType=20,userId=('"+req.params.id+"')",
+   headers: {
+    "Authorization" : auth
+  },
+  json: true,
+  gzip: true
+};
+
+rp(requestOptions).then(response => {
+  console.log('API call response:', response);
+    res.send(response);
+}).catch((err) => {
+  console.log('API call error:', err.message);
+});
+})
